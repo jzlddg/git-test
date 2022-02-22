8d201cc2a2331ba93fd9a406b62cda36621b3303
jzl <jzl@qq.com>
2022-02-22 20:34:01 +0800

    first commit

---
+++ src/main/java/com/jzl/Test/GitTest.java
@@ -0,0 +1,11 @@
+package com.jzl.Test;
+
+/**
+ * @author 蒋忠利
+ * @create 2022-02-22-20:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
